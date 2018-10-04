3c9cad5a97679146d203d5cbcce63028ba9011ac
Ju Demarque <ju.dmrq@gmail.com>
2018-10-04 07:51:59 -0300

    Add font-family property to InputField and Button atoms

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Button.module.css';
 
 export const Button = ({disabled, children}) => (
-  <button className={disabled ? styles.disabled : styles.primary} disabled={disabled}>
+  <button className={styles.primary} disabled={disabled}>
     {children}
   </button>
 );
