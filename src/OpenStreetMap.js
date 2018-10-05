ea596f215a78dbbc65b278e541d4f0ad80154ea3
Ju Demarque <ju.dmrq@gmail.com>
2018-10-04 23:19:16 -0300

    Add DefaultIcon to Marker component
    Alternative work around to fix missing images bug

--- src/OpenStreetMap.js
+++ src/OpenStreetMap.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import L from 'leaflet';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const DefaultIcon = new L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconAnchor: [10, 46],
+});
 
 export const OpenStreetMap = ({latitude, longitude, zoom = 4}) => {
   const lat = latitude || -10.3333,
@@ -10,9 +20,9 @@ export const OpenStreetMap = ({latitude, longitude, zoom = 4}) => {
     <Map center={position} zoom={zoom}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png'
       />
-      <Marker position={position} />
+      <Marker icon={DefaultIcon} position={position} />
     </Map>
   );
 }
